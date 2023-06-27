5fa143de52ff238da589904f31116a73efa82190
Sagar Patel <16679749+sagarp-patel@users.noreply.github.com>
2023-06-27 14:35:52 -0400

    Adding parallel processing to encode_text to allow for higher workloads

--- cephalon/src/models/model.rs
+++ cephalon/src/models/model.rs
@@ -2,6 +2,7 @@
 ///Use Case: Use this to create embeddings for sentences.   
 #[cfg(not(feature="no-ml"))]
 pub fn encode_text(text:&Vec<String>)->Option<Vec<Vec<f32>>>{
+    use rayon::prelude::*;
     use rust_bert::pipelines::sentence_embeddings::{
         SentenceEmbeddingsBuilder,
         SentenceEmbeddingsModel, SentenceEmbeddingsModelType
@@ -10,19 +11,34 @@ pub fn encode_text(text:&Vec<String>)->Option<Vec<Vec<f32>>>{
         println!("No text found");
         return None
     }
-    let model:SentenceEmbeddingsModel;
 
-    match SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL6V2)
-    .create_model(){
-            Ok(sentence_embedding_model)=> model = sentence_embedding_model,
-            Err(err)=>panic!("Error Generating Model: {:?}",err)
-        }    
-    match model.encode(text){
-        Ok(encodings)=>{
-            Some(encodings)
-        },
-        Err(err)=>{
-            panic!("Error encoding text: {:?}",err)
-        }
-    }  
+    let mut embedded_vector:Vec<Vec<Vec<f32>>> = vec![vec![],vec![],vec![],vec![]];
+
+    text.par_chunks(4).zip(&mut embedded_vector).for_each(|(sentences, encoding_vec)| {
+        let model:SentenceEmbeddingsModel;
+
+        match SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL6V2)
+        .create_model(){
+                Ok(sentence_embedding_model)=> model = sentence_embedding_model,
+                Err(err)=>panic!("Error Generating Model: {:?}",err)
+            }    
+        
+        for sentence in sentences{
+            match model.encode(&[sentence]){
+                Ok(mut encodings)=>{
+                    encoding_vec.append(&mut encodings);
+                },
+                Err(err)=>{
+                    panic!("Error encoding text: {:?}",err)
+                }
+            }
+        }  
+    });
+
+    let mut output_vec:Vec<Vec<f32>> = vec![];
+    for mut slice in embedded_vector{
+        output_vec.append(&mut slice);
+    }
+
+    Some(output_vec)
 }
